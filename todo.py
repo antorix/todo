6a1a374a3088c80bdac8dbf23661234e44268770
Anton Chivchalov <antorix@gmail.com>
2017-11-03 09:37:45 +0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# This is a very simple to-do list for Android. Requires QPython3 (download it from Google Play Market).
+# This is a very simple to-do list for Android. Requires QPython3 to run (download it from Google Play Market).
 
 from androidhelper import Android
 droid = Android()
